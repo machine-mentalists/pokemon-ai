8a090f90e6c439aaa6ab45e0a9a5b455916bb8cd
Julien Midedji <Julien.Midedji@gmail.com>
2024-10-09 21:34:39 +0200

    Add coordinates

--- testo.lua
+++ testo.lua
@@ -1,3 +1,11 @@
-for i=0x0, 0x5, 0x1 do
+console.clear()
+
+-- Name
+for i=0x0, 0x7, 0x1 do
 	print(string.format("%X", tonumber(memory.read_u8(0x2598+0xABC0+i,"System Bus"))))
 end
+
+-- Coordinates
+x = string.format("%X", tonumber(memory.read_u8(0x260D+0xAD54, "System Bus")))
+y = string.format("%X", tonumber(memory.read_u8(0x260E+0xAD54, "System Bus")))
+print(x .. ", " .. y)
