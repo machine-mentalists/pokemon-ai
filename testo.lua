d55e89b649a65d47b71fd6113868daf235d0f193
Schizzler <salexander11@icloud.com>
2024-10-09 20:25:47 +0200

    Player name now accessible

--- testo.lua
+++ testo.lua
@@ -1,10 +1,16 @@
+-- Source: https://github.com/mkdasher/PokemonBizhawkLua/blob/master/pkmgen3
+
 local utils = {} --wegen dofile
 function utils.translatePath(path)
 	local separator = package.config:sub(1, 1)
 	local pathTranslated = string.gsub(path, "\\", separator)
 	return pathTranslated == nil and path or pathTranslated
 end --wegen dofile
-dofile (utils.translatePath("Memory.lua"))
-local trainer = Memory.readdword(0x25B6)
---for i=0,2000,1 do print(Memory.readdword(i)) end
-print(trainer)
+--dofile (utils.translatePath("Memory.lua"))
+--local trainer = Memory.readbyte(0x2598)
+local f = assert(loadfile(utils.translatePath("Memory.lua")))
+--for i=0x0,0x60,0x1 do print(i, Memory.readbyte(Memory.readbyte(i+0x2598+0xABC0))) end
+--for i=0x0,0x60,0x1 do print(i, string.format("%X", tonumber(Memory.readbyte(i+0x2598+0xABC0)))) end
+for i=0x0, 0x5, 0x1 do
+	print(string.format("%X", tonumber(memory.read_u8(0x2598+0xABC0+i,"System Bus"))))
+end
